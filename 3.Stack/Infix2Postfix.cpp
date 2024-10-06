e33409b0a4e2c1396d7d82a3f7ac27bb2a0a2b57
BelokanDEV <huffam2522@gmail.com>
2024-10-07 03:10:32 +0900

    10.07

--- 3.Stack/Infix2Postfix.cpp
+++ 3.Stack/Infix2Postfix.cpp
@@ -1,5 +1,7 @@
 // Infix2Postfix.cpp
 #include "ArrayStack.h"
+
+//if A*B+C => AB*C+
 //if (A+B)*C => AB+C*
 
 inline int precedence ( char op ){
@@ -13,17 +15,42 @@ inline int precedence ( char op ){
 
 void infix2Postfix( FILE *fp = stdin ){
     char c, op;
-    double var;
+    double val;
     ArrayStack st;
 
-    while((c=getc(fp)) != '\n' ){
-        if (c == '+' || c == '-' || c == '*' || c == '/' || c == '(' || c == ')'){
-            op = c;
-            for(int i = 0; i<= st.data[top]; i++){
-                if( precedence(op) < precedence(st.data[i]) )
+    while ( (c=getc(fp)) != '\n' ) {
+        if((c>='0') && (c<='9')){
+            ungetc( c, fp ); // c가 숫자일경우 fp에 반환
+            fscanf( fp, "%lf", &val); // fp에 입력되어있는 숫자를(한자리 이상의 숫자 또한) 실수 형태로 val 형태로 저장
+            printf("%4.1f", val);
+        }
+
+        else if ( c == '(') { st.push(c); }
+        else if ( c == ')') {
+            while (!st.isEmpty()) {
+                op = st.pop();
+                if( op == '(') break;
+                else printf( "%c ", op);
             }
         }
+        
+        else if ( c == '+' || c == '-' || c == '/' || c == '*'){
+            while (!st.isEmpty()){
+            op = st.peek();
+            if ( precedence(c) <= precedence(op) ) {
+                printf( "%c ", op);
+                st.pop();
+            }
+            else { break; }
+        }
+        st.push(c);
+        }
     }
+    while (!st.isEmpty()) { printf("%c ", st.pop() ); }
+    printf( "\n");
 }
 
-
+int main() {
+    printf("수식 입력 (Infix) = ");
+    infix2Postfix();
+}
