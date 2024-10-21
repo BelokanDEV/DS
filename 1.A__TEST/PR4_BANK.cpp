b9786de3f85e55363ee3c07e7f958e1d7ede419b
BelokanDEV <huffam2522@gmail.com>
2024-10-22 00:16:00 +0900

    1

--- 1.A__TEST/PR4_BANK.cpp
+++ 1.A__TEST/PR4_BANK.cpp
@@ -1,8 +1,8 @@
 #include <cstdio>
 #include <cstdlib>
-#include <time.h>
+#include <ctime>
 
-#define MAX_QUEUE_SIZE 100
+#define MAX_QUEUE_SIZE 20
 inline void error(char* str){
     printf("%s", str);
     exit(1);
@@ -12,29 +12,31 @@ struct Customer{
     int id;
     int tArrival;
     int tService;
-    Customer(int i, int tArr, int tSer)
+    Customer(int i = 0, int tArr = 0, int tSer = 0)
     : id(i), tArrival(tArr), tService(tSer){}
-}
+};
 
 class CustomerQueue{
     Customer data[MAX_QUEUE_SIZE];
     int front;
     int rear;
 public :
+    CustomerQueue()
+    : front(0), rear(0) {}
     bool isEmpty(){ front == rear; }
-    void isFull(){ front == (rear+1)%MAX_QUEUE_SIZE; }
+    bool isFull(){ front == (rear+1)%MAX_QUEUE_SIZE; }
     void enqueue(Customer n){
-        if( isFull( )) { error("STACK IS FULL"); }
+        if( isFull( )) { error("QUEUE IS FULL"); }
         rear = (rear+1)%MAX_QUEUE_SIZE;
         data[rear] = n;
     }
     Customer dequeue(){
-        if( isEmpty( )) { error("STACK IS EMPTY")}
-        Customer temp = data[front];
+        if( isEmpty( )) { error("QUEUE IS EMPTY"); }
         front = (front+1)%MAX_QUEUE_SIZE;
-        return temp;
+        return data[front];
     }
-}
+};
+
 class BankSimulator{
     int nSimulation;
     double probArrival;
@@ -45,12 +47,12 @@ class BankSimulator{
     int nCustomers;
     CustomerQueue que;
 
-    double Random(){ rand()/(double)RAND_MAX; }
-    bool isNewCustomer() { probArrival>random(); }
+    double Random(){ return rand()/(double)RAND_MAX; }
+    bool isNewCustomer() { return probArrival>Random(); }
     int randServicetime() {return (int)(Random()*tMaxService)+1; }
     void insertCustomers(int arrivalTime){
         Customer a(++nCustomers, arrivalTime, randServicetime());
-        printf("고객 <%d> 방문, 서비스 시간 : ",a.id, a.tService);
+        printf("고객 <%d> 방문, 서비스 시간 : %d\n",a.id, a.tService);
         que.enqueue(a);
     }
 public :
@@ -69,6 +71,7 @@ public :
         int serviceTime = -1;
         while(clock<nSimulation){
             clock++;
+            printf("현재시각 = %d\n",clock);
             if(isNewCustomer()) {insertCustomers(clock);}
             if(serviceTime>0) {serviceTime--; }
             else{
@@ -76,7 +79,7 @@ public :
                 Customer a = que.dequeue();
                 nServedCustomers++;
                 totalWaitTime += clock-a.tArrival;
-                printf("고객 <%d> 서비스 시작!, 대기시간 : ", a.id, clock-a.tArrival);
+                printf("고객 <%d> 서비스 시작!, 대기시간 : %d\n", a.id, clock-a.tArrival);
                 serviceTime = a.tService-1;
             }
         }
@@ -88,10 +91,13 @@ public :
         printf("    서비스고객 평균대기시간 = %-5.2f분\n",(double)totalWaitTime/nServedCustomers);
         printf("    현재 대기 고객 수 = %d\n",nCustomers - nServedCustomers);
     }
+};
 
-    
-
-}
-int main(void){
-
+int main() {
+    srand( (unsigned int) time(NULL) );
+    BankSimulator sim;
+    sim.readSimulationParameters();
+    sim.run();
+    sim.printStats();
+    return 0;
 }
